b12d613a0f048667a40e1361a990233c09566ddb
dell <zhouweiyong55@163.com>
2019-04-09 10:23:49 +0800

    init

---
+++ transaction/src/main/java/com/zwy/transaction/utils/DbUtils.java
@@ -0,0 +1,52 @@
+package com.zwy.transaction.utils;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbUtils {
+    private ComboPooledDataSource dataSource;
+
+    public DbUtils() {
+        this.dataSource = new ComboPooledDataSource();
+    }
+
+    public Connection getConn() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public void close(Connection conn, PreparedStatement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (rs != null)
+                rs.close();
+            if (ps != null)
+                rs.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 关闭连接池
+     */
+    public void closePool() {
+        dataSource.close();
+    }
+}
