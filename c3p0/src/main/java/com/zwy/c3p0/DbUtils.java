b14413506545d67c070c841149ebdd44d678ecbb
dell <zhouweiyong55@163.com>
2019-04-09 10:21:44 +0800

    transaction

--- c3p0/src/main/java/com/zwy/c3p0/DbUtils.java
+++ c3p0/src/main/java/com/zwy/c3p0/DbUtils.java
@@ -1,4 +1,53 @@
 package com.zwy.c3p0;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DbUtils {
+    private ComboPooledDataSource dataSource;
+
+    public DbUtils() {
+        this.dataSource = new ComboPooledDataSource();
+    }
+
+    public Connection getConn() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public void close(Connection conn, PreparedStatement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (rs != null)
+                rs.close();
+            if (ps != null)
+                rs.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 关闭连接池
+     */
+    public void closePool() {
+        dataSource.close();
+    }
 }
--- c3p0/src/test/java/com/zwy/c3p0/C3p0XmlTest.java
+++ c3p0/src/test/java/com/zwy/c3p0/C3p0XmlTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * 通过xml配置连接池
@@ -31,12 +32,38 @@ public class C3p0XmlTest {
 
                 System.out.println("id:" + id + " name:" + name + " price:" + price);
             }
-
             ps.close();
             conn.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void findAll() {
+        DbUtils dbUtils = new DbUtils();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = dbUtils.getConn();
+            String sql = "select * from product";
+            ps = conn.prepareCall(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+
+                System.out.println("id:" + id + " name:" + name + " price:" + price);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }finally {
+            dbUtils.close(conn, ps, rs);
+        }
+
     }
 }
